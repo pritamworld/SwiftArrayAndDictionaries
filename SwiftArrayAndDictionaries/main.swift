fd4105d17e31ad3543e19adf8b590d9b62a65654
moxDroid Labs Inc <pritam.world@gmail.com>
2019-02-08 18:13:02 -0500

    Initial Commit

---
+++ SwiftArrayAndDictionaries/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftArrayAndDictionaries
+//
+//  Created by moxDroid on 2019-02-08.
+//  Copyright © 2019 moxDroid. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
